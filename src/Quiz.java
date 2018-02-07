4c345c90c2916b9ab28054d6ae89fbb732ef0c83
amel22 <amelkaric1989@gmail.com>
2018-02-07 11:45:43 +0100

    My bad

--- src/Quiz.java
+++ src/Quiz.java
@@ -37,22 +37,4 @@ public class Quiz {
 	}
 	
 	
-	public static int sabiranje (int a, int b){
-		return a+b;
-	}
-	public static int oduzimanje (int a, int b){
-		return a-b;
-	}
-	public static int mnozenje (int a, int b){
-		return a*b;
-	}
-	public static int dijeljenje(int a, int b){
-		return a/b;
-	}
-	public static double korjenovanje(int a){
-		return Math.sqrt(a);
-	}
-	public static int kvadriranje(int a){
-		return a*a;
-		}
 }
