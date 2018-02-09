97caf857e5d5bacfa9d58d26b89104d378f4ac2a
EmirHusanovic <emirhusanovic@outlook.com>
2018-02-09 09:42:09 +0100

    Ko fol uradio

--- src/brojevi/Brojevi.java
+++ src/brojevi/Brojevi.java
@@ -4,6 +4,25 @@ public class Brojevi {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+
 	}
 
+	public static void primeNum() {
+		for (int i = 0; i < 1000; i++) {
+			if (isPrime(i)) {
+				System.out.println(i + " ");
+			}
+		}
+	}
+
+	public static void emirp() {
+		int count = 0, n = 2;
+		while (count < 100) {
+			if ((!isPalindrom(n) && isPrime(n))) {
+				count++;
+				System.out.print(n + " ");
+			}
+			n++;
+		}
+	}
 }
