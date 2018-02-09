e4b8828d28d567aa38e8563ba0bb96ec036626c0
AdemHodzic <athos.blade@gmail.com>
2018-02-09 09:28:46 +0100

    Main zavrsen

--- src/brojevi/Brojevi.java
+++ src/brojevi/Brojevi.java
@@ -1,9 +1,45 @@
 package brojevi;
 
+import java.util.*;
+
 public class Brojevi {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Izaberite opciju kouj zelite da ispisemo: ");
+		int opcija = input.nextInt();
+		switch(opcija){
+		case 1:
+			primeNum();
+			break;
+		case 2:
+			emirp();
+			break;
+		case 3:
+			fibonacciBrojevi();
+			break;
+		case 4:
+			armstrongoviBrojevi();
+			break;
+		case 5:
+			marsennePrime();
+			break;
+
+		}
+		input.close();
 	}
 
+	public static void marsennePrime(){
+		for(int i = 0;i<64;i++){
+			if(isPrime(i) && (i-1)%2==0) System.out.print(i + " ");
+		}
+	}
+	
+	public static boolean isPrime(int num){
+		if(num%2==0)return false;
+		for(int i = 3;i<num;i+=2){
+			if(num%i==0)return false;
+		}
+		return true;
+	}
 }
