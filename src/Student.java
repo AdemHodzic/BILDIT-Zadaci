285b2cf38049a087e3ab3ee5e556f2d32e314f7c
Dzenita Dulovic <djulovic.dzenita1999@gmail.com>
2018-02-09 11:41:23 +0100

    Student

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+	
+	
+	final String STATUS;
+
+	public Student(String status) {
+		this.STATUS = status;
+	}
+	public String toString(){
+		return "Class status: " + this.STATUS;
+	}
+
+}
